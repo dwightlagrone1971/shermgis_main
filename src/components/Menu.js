b0e96d12485120c23122b7dec5dcd7c1f4e79aa4
Dwight LaGrone <dwightlagrone@yahoo.com>
2022-05-23 10:59:10 -0500

    Commit - 052322 v3

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ import Grid from '@mui/material/Grid';
 const items = [
     { name: "Featured Maps", to: "/" },
     { name: "GIS Downloads", to: "/downloads" },
-    { name: "Infographics", to: "/graphics" },
+    { name: "Infographics", to: "/graphics", },
     { name: "Internal Maps", to: "/internal" },
     { name: "PDF Maps", to: "/maps" },
     { name: "About", to: "/about" },
@@ -18,20 +18,56 @@ function Menu() {
   return (
     <div className="menu">
       <nav className="menu__container">
-
         <Grid container spacing={6} rowSpacing={3}>
-          {items.map((item, idx) => (
-            <Grid item xs={12} md={2}>
-              <Link 
-                to={item.to} 
-                style={{ textDecoration: 'none' }}      
-              >
-                <Button className="menu__btn">{item.name}</Button>
-              </Link>
-            </Grid>
-            ))}
-        </Grid> 
-
+          <Grid item xs={12} md={2}>
+            <Link 
+              to="/"
+              style={{ textDecoration: 'none' }}      
+            >
+              <Button className="menu__btn">Featured Maps</Button>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Link 
+              to="/downloads"
+              style={{ textDecoration: 'none' }}      
+            >
+              <Button className="menu__btn">GIS Downloads</Button>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Link 
+              to="/graphics"
+              style={{ textDecoration: 'none' }}      
+            >
+              <Button className="standout__btn">Infographics</Button>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Link 
+              to="/internal"
+              style={{ textDecoration: 'none' }}      
+            >
+              <Button className="menu__btn">Internal Maps</Button>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Link 
+              to="/maps"
+              style={{ textDecoration: 'none' }}      
+            >
+              <Button className="menu__btn">PDF Maps</Button>
+            </Link>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Link 
+              to="/about"
+              style={{ textDecoration: 'none' }}      
+            >
+              <Button className="menu__btn">About</Button>
+            </Link>
+          </Grid>
+        </Grid>        
       </nav>
     </div>
   ) 
