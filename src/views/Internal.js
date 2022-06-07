8fe14a140a502ba9c2d1ef33bcb56471e1e85ad7
Dwight LaGrone <dwightlagrone@yahoo.com>
2022-06-07 09:02:18 -0500

    Commit - 060722 v1

--- src/views/Internal.js
+++ src/views/Internal.js
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid';
 
 const items = [
+  { name: "9-1-1 Addressing", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=ab4fff47a6074a9f8efc19821359cdca" },
   { name: "Code Incidents (2022)", href: "https://cityofsherman.maps.arcgis.com/apps/webappviewer/index.html?id=83d4528b6352402d9ce262c45a07e11a" },
   { name: "Code Incidents (All)", href: "https://cityofsherman.maps.arcgis.com/apps/webappviewer/index.html?id=2e9d062f4c6e4cac9ec10024ebc8eb7b" },
   { name: "Code Simple Map", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=97013d9eff694c059406229d9df88b37" },
