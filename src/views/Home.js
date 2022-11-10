b52ea78eed0ea4e719eceafef26dfd3036b94f89
Dwight LaGrone <dwightlagrone@yahoo.com>
2022-11-10 11:46:03 -0600

    Commit - 111022 v2

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,6 +7,8 @@ import Grid from '@mui/material/Grid';
 const items = [
   { name: "ALL-IN-ONE", href: "https://cityofsherman.maps.arcgis.com/apps/webappviewer/index.html?id=b3d663dbb6fa4b75b3ede3131ae38986" },
   { name: "Residential Dashboard", href: "https://cityofsherman.maps.arcgis.com/apps/instant/lookup/index.html?appid=efd3c1d6d3144c9eb11326ec20c79626" },
+  { name: "Future Landuse (2022)", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=dbedffd8809640808078dea3f7db0f4c" },
+  { name: "Thoroughfares (2022)", href: "https://cityofsherman.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=5dbc6809eaa746e5909b8e82fe24d602" },
   // { name: "Address Request", href: "https://survey123.arcgis.com/share/9933e2aecb8e4e97b4107523b70b9e03?portalUrl=https://cityofsherman.maps.arcgis.com" },
 ];
 
@@ -17,7 +19,7 @@ function Home() {
       
       <Grid className="main_body" container spacing={6}>
         {items.map((data, idx) => (
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} s={6} md={3}>
             <a 
               target="_blank"
               href={data.href}       
@@ -31,7 +33,7 @@ function Home() {
         <Link 
           to="/external"            
         >
-          <Button className="more__btn" >MoreMaps</Button>
+          <Button className="more__btn" >More Maps</Button>
         </Link>
       </div>
     </div>     
