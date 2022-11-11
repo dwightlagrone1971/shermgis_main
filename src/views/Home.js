ba38be00ebb57e1b7dd2fddedb22c6d108933471
Dwight LaGrone <dwightlagrone@yahoo.com>
2022-11-11 08:04:48 -0600

    Commit - 111122 v1

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid';
 
 const items = [
-  { name: "ALL-IN-ONE", href: "https://cityofsherman.maps.arcgis.com/apps/webappviewer/index.html?id=b3d663dbb6fa4b75b3ede3131ae38986" },
+  { name: "ALL-IN-ONE", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=af7d99d16f534760951983816168c082" },
   { name: "Residential Dashboard", href: "https://cityofsherman.maps.arcgis.com/apps/instant/lookup/index.html?appid=efd3c1d6d3144c9eb11326ec20c79626" },
   { name: "Future Landuse (2022)", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=dbedffd8809640808078dea3f7db0f4c" },
   { name: "Thoroughfares (2022)", href: "https://cityofsherman.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=5dbc6809eaa746e5909b8e82fe24d602" },
@@ -16,8 +16,7 @@ function Home() {
   return (
     <div className="home"> 
       <h1>Featured Maps</h1>
-      <h3>*Some of the GIS Web Maps are not working today.  There will be updates tonight and the maps should be working tomorrow.  Sorry for the inconvenience.</h3>
-      
+       
       <Grid className="main_body" container spacing={6}>
         {items.map((data, idx) => (
           <Grid item xs={12} s={6} md={3}>
