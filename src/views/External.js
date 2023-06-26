ed2212414e9f6c68ed309dd22e17a6133a119cd0
Dwight LaGrone <dwightlagrone@yahoo.com>
2023-06-26 15:54:58 -0500

    Commit 062623 v1

--- src/views/External.js
+++ src/views/External.js
@@ -8,9 +8,11 @@ const items = [
   { name: "Council District", href: "https://experience.arcgis.com/experience/97084b091f5d4972bc5d52d407a8cbab/?draft=true" },
   { name: "Emergency Sirens", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=d54707ac56ee4971ab757339883af01a" },
   { name: "Flood Zones (2023)", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=a8237551de654fa1ab568ad83c685253" },
+  { name: "Future Landuse", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=dbedffd8809640808078dea3f7db0f4c" },
   { name: "Parks & Recreation", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=f60017513bbf45f4b2197074a2e6c2c7" },
   { name: "Sex Offender Restricted", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=446adf191c764b3e83a116072a77979d" },
   { name: "Sherman Schools", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=a51f81f031d94df884004a01d5da5932" },
+  { name: "Thoroughfares", href: "https://cityofsherman.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=5dbc6809eaa746e5909b8e82fe24d602" },
   { name: "Trash Pickup Days", href: "https://cityofsherman.maps.arcgis.com/apps/instant/lookup/index.html?appid=000839d23a014bd791bb19d4667f177c" },
   { name: "Zoning Districts", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=d0d3a59dfbe2427d94a512bc5a586270" },
 ];
@@ -19,7 +21,7 @@ const items = [
 function External() {
   return (
     <div class=""> 
-      <h1 class="font-serif text-4xl sm:mt-10 mb-10 lg:mt-10 lg:mb-4 lg:text-5xl">Public Maps</h1>
+      <h1 class="font-serif text-4xl sm:mt-8 mb-2 lg:mt-2 lg:mb-2 lg:text-5xl">Public Maps</h1>
 
       <Grid class="h-full lg:w-9/12 lg:pt-2 lg:m-auto grid lg:grid-cols-3 lg:gap-2 lg:w-6/12 lg:text-center">
           {items.map((items, idx) => (
@@ -27,7 +29,7 @@ function External() {
                 target="_blank"
                 href={items.href}       
               >
-                <Button class="font-serif bg-neutral-700 hover:bg-neutral-500 text-neutral-200 mb-6 w-52 py-1 md:w-52">{items.name}</Button>
+                <Button class="font-serif bg-neutral-700 hover:bg-neutral-500 text-neutral-200 mb-4 w-52 py-1 md:w-52">{items.name}</Button>
               </a>
             ))}          
         </Grid>
--- src/views/Home.js
+++ src/views/Home.js
@@ -7,17 +7,16 @@ import Grid from '@mui/material/Grid';
 const items = [
   { name: "ALL-IN-ONE", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=af7d99d16f534760951983816168c082" },
   { name: "Residential Dashboard", href: "https://cityofsherman.maps.arcgis.com/apps/instant/lookup/index.html?appid=efd3c1d6d3144c9eb11326ec20c79626" },
-  { name: "Future Landuse", href: "https://cityofsherman.maps.arcgis.com/apps/instant/sidebar/index.html?appid=dbedffd8809640808078dea3f7db0f4c" },
-  { name: "Thoroughfares", href: "https://cityofsherman.maps.arcgis.com/apps/instant/interactivelegend/index.html?appid=5dbc6809eaa746e5909b8e82fe24d602" },
+  { name: "Council District", href: "https://experience.arcgis.com/experience/97084b091f5d4972bc5d52d407a8cbab/?draft=true" },
 ];
 
 function Home() {
   return (
     <>
       <div>
-        <h2 class="font-serif text-4xl mt-10 mb-10 lg:mt-10 lg:mb-4 lg:text-5xl">Featured Maps</h2>
+        <h2 class="font-serif text-4xl mt-10 mb-10 lg:mt-14 lg:mb-4 lg:text-5xl">Featured Maps</h2>
 
-        <Grid class="auto grid lg:m-auto lg:pt-16 lg:grid-cols-4 lg:gap-2 lg:w-7/12 lg:text-center">
+        <Grid class="h-full lg:w-9/12 lg:pt-12 lg:m-auto grid lg:grid-cols-3 lg:gap-2 lg:w-6/12 lg:text-center">
           {items.map((items, idx) => (
               <a 
                 target="_blank"
