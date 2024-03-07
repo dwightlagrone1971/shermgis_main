6806b0b9fa36f1e35c7bb3637b31f486cc2cc00c
Dwight LaGrone <dwightlagrone@yahoo.com>
2024-03-07 14:37:24 -0600

    Commit - 030724 v1

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,7 +7,7 @@ import Grid from '@mui/material/Grid';
 const items = [
   { name: "ALL-IN-ONE", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/cb19b06c606f4ce1bd9e96b8817d4e07" },
   { name: "Residential Dashboard", href: "https://cityofsherman.maps.arcgis.com/apps/instant/lookup/index.html?appid=efd3c1d6d3144c9eb11326ec20c79626" },  
-  { name: "City Projects Site (2024)", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/7ef89fd885e04d8e82d4ff2195a94b94" },
+  { name: "City Projects Site (New)", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/7ef89fd885e04d8e82d4ff2195a94b94" },
   { name: "City Projects Site", href: "https://experience.arcgis.com/experience/edf1ffbd9dae4b12aeefc8b1ba594b26" },
 ];
 
