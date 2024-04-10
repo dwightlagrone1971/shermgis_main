5fcd6c437cf3f228ec337034fb8e7a07497108f4
Dwight LaGrone <dwightlagrone@yahoo.com>
2024-04-10 13:03:52 -0500

    Commit - 041024 v1

--- src/views/External.js
+++ src/views/External.js
@@ -5,15 +5,15 @@ import Grid from '@mui/material/Grid';
 
 const items = [
   { name: "Annexations", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/2848ecc1229d454fa962392967548752" },
-  { name: "Capital Improvements", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/1be8bb7a791645809e4cdc07d77c8178" },
+  { name: "Capital Improvements", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/818c88c6c1264590810cb91a0db2c24a" },
   { name: "City Projects Site", href: "https://experience.arcgis.com/experience/edf1ffbd9dae4b12aeefc8b1ba594b26" },
   { name: "Council District", href: "https://experience.arcgis.com/experience/f2ead96f4ef14f19bf2e23767b65d200/" },
-  { name: "Development Projects", href: "https://experience.arcgis.com/experience/bf7f869e182c42df876a2e667559e24a/page/Development-Projects/?data_id=dataSource_2-18a242207a2-layer-5%3A35&draft=true&views=Proposed-" },
+  { name: "Development Projects", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/5628948f1c1a464da61cc0bf966badf0" },
   { name: "Emergency Sirens", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/a3685004829d4465abd34e067f30c5fb" },
   { name: "Flood Zones (2023)", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/0544b4d51106433ba1c0637fb2c5f724" },
   { name: "Future Landuse", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/f408802be3f04be2a2c699a033a878b3" },
   { name: "Parks & Recreation", href: "https://experience.arcgis.com/experience/7f30c9436abd408391d472171b504f3c/?draft=true" },
-  { name: "Planning & Zoning", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/0f075ea148cf4eb1818a73566114fa7b" },
+  { name: "Planning & Zoning", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/da82c92929e34750a26d95d5f8738cc6" },
   { name: "Sex Offender Restricted", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/7797ab097a38493da51128f124d91370" },
   { name: "Sherman Schools", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/ba7afb45ee6d4e028fe81721efae9d01" },
   { name: "Thoroughfares", href: "https://cityofsherman.maps.arcgis.com/apps/dashboards/3b28c17a8cac472ab8ff18b0576cacfe" },
